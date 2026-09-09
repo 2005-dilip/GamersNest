f650ac69229f14dc31b3127d05746670ca709222
2005-dilip <dilipkumar.26291111@gmail.com>
2026-09-09 09:13:04 +0530

    Remove gamersnest-angular from git tracking and add to .gitignore

--- gamersnest-angular/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: '404', component: NotFoundComponent },
-  { path: '**', component: NotFoundComponent },
-];
